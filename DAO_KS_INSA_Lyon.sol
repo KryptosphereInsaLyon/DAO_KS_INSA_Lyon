6bd15c5af524181e1a52e6726456f82a2d3921db
ClementGlr <119356506+ClementGlr@users.noreply.github.com>
2023-01-09 21:53:26 +0100

    Update DAO_KS_INSA_Lyon.sol

--- DAO_KS_INSA_Lyon.sol
+++ DAO_KS_INSA_Lyon.sol
@@ -10,6 +10,7 @@ contract DAO_KS_INSA_Lyon is KSIL{
         string nom;
         uint id;
         uint dateDeCreation;
+        bool status;
         uint16 pour;
         uint16 contre;
     }
@@ -18,6 +19,8 @@ contract DAO_KS_INSA_Lyon is KSIL{
     uint _propCompteur = 0;
 
     uint public prixCreerVote = 2;
+    
+    address master = selectionMaster();
 
     constructor() {
         propositions.push(Proposition({
@@ -27,9 +30,11 @@ contract DAO_KS_INSA_Lyon is KSIL{
             dateDeCreation: block.timestamp,
             pour: 2,
             contre: 1
+            status : true
         }));
         _propCompteur += 1;
     }
+    
 
     //un pelo propose un vote et paye prixCreerVote
     function creerVote(string memory _nom) public {
@@ -40,26 +45,41 @@ contract DAO_KS_INSA_Lyon is KSIL{
             dateDeCreation: block.timestamp,
             pour: 0,
             contre: 0
+            status : true
         }));
         _propCompteur += 1;
+    }
+    
+    modifier master() {
+        require(msg.sender == master );
+        _;
     }
 
     //choisi un master parmi les plus riches
     function selectionMaster() public {}
 
     //le master attribue les votes à la fin de la séance
-    function attributionKIL() public {}
+    function attributionKIL(string nom, uint256 amount) public master {
+        _mint(nomToAddress[nom], amount);
+    }
 
     //le vote a lieu, le créateur ne peut pas voter
     function voter(uint _propId, uint16 _nbKIL, bool _choix) public {
-        //Le créateur ne peut pas voter
-        require (propositions[_propId].createur == msg.sender);
-        //Ajout des pour/contre et dépense des jetons de l'appelant
-        transfer(address(this), _nbKIL);
-        if (_choix) {
-            propositions[_propId].pour += _nbKIL;
-        } else {
-            propositions[_propId].contre += _nbKIL;
+         if(propositions[_propId].dateDeCreation + 604800 <= block.timestamp){ 
+            //Le créateur ne peut pas voter
+            require (propositions[_propId].createur == msg.sender);
+            //Ajout des pour/contre et dépense des jetons de l'appelant
+            transfer(address(this), _nbKIL);
+            if (_choix) {
+                propositions[_propId].pour += _nbKIL;
+            } else {
+                propositions[_propId].contre += _nbKIL;
+            }
         }
+    }
+    
+    //permet d'ajouter un membre de l'asso
+    function ajouterMembre(string _nom, address _adresse) public master {
+        nomToAddress[_nom] = _adresse;
     }
 }
